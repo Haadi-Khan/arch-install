3543f697e0c64a1d2e4440f21f9a48457ada8a2e
Haadi-Khan <44217975+Haadi-Khan@users.noreply.github.com>
2022-05-18 20:38:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,24 @@ then
     rm -rf ~/.config
     git clone https://github.com/Haadi-Khan/.config.git ~/.config
 fi
+
+echo "You have now completed installing the base programs and config
+Next, here are some additional utilities which are needed for the system
+to function properly"
+
+read -p "Install the additional utilities? [y/N]? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git clone https://github.com/nan0s7/nfancurve.git
+    git clone https://github.com/rizmut/libreoffice-style-colibre
+    git clone https://github.com/Diolinux/PhotoGIMP
+fi
+
+echo "Now, go and install the appropriate files for nfancurve,
+libreoffice icons, and photogimp. Go to their respective folders in ~
+"
+
+echo "Almost done. Just remember to get Badlion Client's appimage
+since you're a MC sweat, as well as installing your steam games.
+Enjoy setting up the rest of the programs which don't just have configs"
